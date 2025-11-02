ce57590fba33f2f73f5c9d1ea63880eb1a9f0b8b
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2025-11-02 20:11:18 +0530

    Demon Thread

--- src/DemonThread.java
+++ src/DemonThread.java
@@ -1,2 +1,45 @@
 public class DemonThread {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new DemonHelper());
+        Thread thread2 = new Thread(new UserThread());
+        thread1.setDaemon(true);
+        thread1.start();
+        thread2.start();
+    }
+
+
+}
+class  DemonHelper implements Runnable{
+
+    @Override
+    public void run() {
+        int count = 0 ;
+        while (count < 100){
+            count++;
+            System.out.println("From demon thread :"+count );
+            try {
+                Thread.sleep(1000); //sleep is the static method
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+
+        }
+    }
+
+}
+
+class UserThread implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+        System.out.println("end of execution of user thread");
+
+    }
 }
